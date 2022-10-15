6639e88ca5f3c29e86eeed398b3ae724c25a2899
josef <josef@cubiclease.com>
2022-10-15 17:54:20 +0100

    Fixed command line calculation

--- Commands/calculateFee.php
+++ Commands/calculateFee.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use Lendable\Interview\Interpolation\Model\FeeCalculator;
 use Lendable\Interview\Interpolation\Model\LoanApplication;
@@ -14,18 +14,18 @@ while (!LoanTermValidator::validateTerm($term)) {
     fscanf(STDIN, "%i\n", $term);
 
     if (!LoanTermValidator::validateTerm($term)) {
-        fwrite(STDOUT, "The term $term is not 12 or 24 months. Please try again.\n");
+        fwrite(STDOUT, "The term entered is not 12 or 24 months. Please try again.\n");
     }
 }
 
 $amount = 0;
 
-while (LoanAmountValidator::validateAmount($amount)) {
+while (!LoanAmountValidator::validateAmount($amount)) {
     fwrite(STDOUT, "Please enter a loan amount between £1000 & £20000\n");
-    fscanf(STDIN, "%d\n", $amount);
+    fscanf(STDIN, "%f\n", $amount);
 
-    if (LoanAmountValidator::validateAmount($amount)) {
-        fwrite(STDOUT, "The amount $amount is not between £1000 & £20000. Please try again.\n");
+    if (!LoanAmountValidator::validateAmount($amount)) {
+        fwrite(STDOUT, "The amount entered is not between £1000 & £20000. Please try again.\n");
     }
 }
 
